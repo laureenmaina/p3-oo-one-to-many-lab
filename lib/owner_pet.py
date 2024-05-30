d1db7e67c4f344807295f5345a889aad8bc0944d
Laurine Maina <mainalaureen58@gmail.com>
2024-05-30 21:40:04 +0300

    initial commit

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,6 +1,5 @@
 class Pet:
-    all=[] #class variable all
-
+    all=[] 
     PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
     def __init__(self,name,pet_type,owner=None):
         self.name=name
@@ -10,8 +9,6 @@ class Pet:
 
         if pet_type not in Pet.PET_TYPES:
             raise Exception(f"Invalid pet type: {pet_type}. Must be one of {Pet.PET_TYPES}.")
-        #Define a class variable PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"] and validate that the pet_type is one of those types in the __init__ method.
-#raise Exception if this check fails.
         if owner is not None and not isinstance(owner, Owner):
             raise TypeError("owner must be an instance of Owner class")
            
@@ -20,15 +17,15 @@ class Owner:
     def __init__(self,name):
         self.name=name
 
-    def pets(self): #returns a full list of the owner's pet
+    def pets(self): 
         return [pet for pet in Pet.all if pet.owner ==self]
     
-    def add_pet(self,pet): #checks that the the pet is of type Pet and adds the owner to the pet.
+    def add_pet(self,pet):
         if isinstance(pet,Pet):
                    TypeError("Owner object has no attribute pet_type")
         pet.owner = self
 
-    def get_sorted_pets(self): # returns a sorted list of pets by their names.
+    def get_sorted_pets(self): 
         pets_list = self.pets()
         pets_list.sort(key=lambda pet: pet.name)
         return pets_list
